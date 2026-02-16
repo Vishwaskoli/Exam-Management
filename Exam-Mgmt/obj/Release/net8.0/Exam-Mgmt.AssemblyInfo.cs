53082fb6d1626ce638fe507a8edf66319e44d8bb
mayurborse543-cmd <mayurborse543@gmail.com>
2026-02-16 16:34:22 +0530

    commit

--- Exam-Mgmt/obj/Release/net8.0/Exam-Mgmt.AssemblyInfo.cs
+++ Exam-Mgmt/obj/Release/net8.0/Exam-Mgmt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exam-Mgmt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b6bda4315a12cbd8c010c398c082cc659ec378a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0eee087bd2b169dc13161fb0b85a90b97325d47")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exam-Mgmt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exam-Mgmt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
