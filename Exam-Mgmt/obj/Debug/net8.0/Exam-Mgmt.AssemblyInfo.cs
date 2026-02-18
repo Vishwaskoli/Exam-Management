90591d9407ee48bc277d66cc136ba006a2244aa2
ShreyashIndulkar <shreyashindulkar78978900@gmail.com>
2026-02-18 18:18:00 +0530

    Before merging to Mayur

--- Exam-Mgmt/obj/Debug/net8.0/Exam-Mgmt.AssemblyInfo.cs
+++ Exam-Mgmt/obj/Debug/net8.0/Exam-Mgmt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exam-Mgmt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28ac8a3a6cbf3b4f5fbefbea39d0f6eb517ece95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a34c58edb5634e6bdef138e1ea357b1975e058fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exam-Mgmt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exam-Mgmt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
