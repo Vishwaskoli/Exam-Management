58a4e8da94b00d6124591676013f4e9fea2a64ef
Vishwaskoli <140147651+Vishwaskoli@users.noreply.github.com>
2026-02-27 16:41:36 +0530

    DeleteStudent

---
+++ Exam-Mgmt/Models/DeleteStudentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Exam_Mgmt.Models
+{
+    public class DeleteStudentDto
+    {
+        public int Student_Id { get; set; }
+        public int Modified_By { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
