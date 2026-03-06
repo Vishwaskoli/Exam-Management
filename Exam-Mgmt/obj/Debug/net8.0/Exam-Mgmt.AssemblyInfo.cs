ee9b993c7e1c1e748ace3ce5c4d71372e0497c83
Chaitanya Mhatre <chaitanyamhatre2349@gmail.com>
2026-03-06 10:44:07 +0530

    pushing to take merge from mayur

--- Exam-Mgmt/obj/Debug/net8.0/Exam-Mgmt.AssemblyInfo.cs
+++ Exam-Mgmt/obj/Debug/net8.0/Exam-Mgmt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exam-Mgmt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+276545d459d2a26c3073cd245dabad0f5325a487")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f85a86e246802edb52e61cbc86353ccbb8948e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exam-Mgmt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exam-Mgmt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
